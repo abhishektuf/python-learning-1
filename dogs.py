a0ac02da7ee0324df8d788fe19f74833cbd73601
Abhishek <contact@abkumar.com>
2020-01-31 15:03:50 +0530

    new changes afternoon friday 31/jan/2020

--- dogs.py
+++ dogs.py
@@ -1,10 +1,11 @@
 class Dog:
     def __init__(self, legs, colour):
-        self.legs=legs
-        self.colour=colour
+        self.legs = legs
+        self.colour = colour
 
-jack=Dog(4, "brown")
-tommy=Dog(3, "black")
+
+jack = Dog(4, "brown")
+tommy = Dog(3, "black")
 
 print(jack)
 print(tommy)
--- tax1.py
+++ tax1.py
@@ -1,5 +1,26 @@
-price = 10
+paid_amount = float(input('Price you paid...: '))
+state = input('Indian state please: ')
 
+
+def state_tax(province, price):
+    if province.lower() == 'haryana':
+        tax = 0.45 * int(price)
+    elif province.lower() == 'delhi':
+        tax = 0.55 * int(price)
+    elif province.lower() == 'rajasthan':
+        tax = 0.76 * int(price)
+    else:
+        tax = 0.18 * int(price)
+    return tax
+
+
+calculated_tax = float(state_tax(state, paid_amount))
+
+total_price = paid_amount + calculated_tax
+
+print('Total price you pay (including all taxes): ' + str(total_price))
+
+'''
 # calculating the tax on food items inside Canada.
 if price >= 1.00:
     tax = .07
@@ -17,3 +38,6 @@ if given_price >= 1.00:
 else:
     tax = 0.00
     print('Tax rate is free :'+str(tax))
+
+
+'''
